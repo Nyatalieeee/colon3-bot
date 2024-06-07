3da76f34c2ed3febdec67b8b04258a5a5daee777
Natalie Raine <nyatalieeee@proton.me>
2024-06-07 01:45:50 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv/config');
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.on('ready', () => {
+    console.log(`${client.user.username} is now online.`);
+});
+
+client.login(process.env.TOKEN);
