767c9b3eb1cc94b2dcb935bedf9686674ae90af5
Nyatalieeee <nyatalieeee@proton.me>
2024-06-07 21:54:17 -0500

    moved quotes to separate module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-require("dotenv/config");
+// import * as config from "../config.json" with { type: "json" };
+// import { Client, GatewayIntentBits } from "discord.js";
 const { Client, GatewayIntentBits } = require("discord.js");
+const quotes = require("./quotes");
+const config = require("../config.json");
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -8,36 +11,6 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ],
 });
-const quotes = [
-    // amelia quotes (21)
-    "> everything that is ballshaped will be consumed \n\n--amelia",
-    ">>> NATALIE \n\ndo not \n\neat \n\ndilators \n\n--amelia",
-    "> the tattoo could just end up being up ur pussy \n\n--amelia",
-    "> like just a pile of boobs \n\n--amelia",
-    "> like can human bodies even DIGEST fetuses \n\n--amelia",
-    "> i didnt know wanting boobs can kill the entirety of europe \n\n--amelia",
-    "> i guess u just need breeding /j \n\n--amelia",
-    "> (it just releases gocks everywhere) \n\n--amelia",
-    "> thats bc you'd be using this only inside nattie \n\n--amelia",
-    "> the julie does NOT know about my habit of eating kids \n\n--amelia",
-    "> just dont put it in \n\n--amelia",
-    "> FOOT SERVER???? [#Unknown] \n\n--amelia",
-    "> bro just invented edible cock \n\n--amelia",
-    "> im going to shit myself \n\n--amelia",
-    "> ok i busted out the foot \n\n--amelia",
-    "> its just pink hole \n\n--amelia",
-    "> MAYBE this head isnt good \n\n--amelia",
-    "> and pussy hair. cant forget pussy hair. \n\n--amelia",
-    '> ig theyd have their dreams of genocide "ruined" by sex \n\n--amelia',
-    "> how would have they uh \n\n> gotten inside \n\n--amelia",
-    "> pussy time :3c \n\n--amelia",
-
-    // natalie quotes (4)
-    "> i did NOT expect to be talking abt blending stolen fetuses today \n\n--natalie",
-    "> wait wtf why are there like holes \n\n--natalie",
-    "> 129 insertions(+) :weary: \n\n--natalie",
-    "> i mean he had a fkn glass jar pop inside his ass and he didnt even SAY anything \n\n--natalie",
-];
 
 client.on("ready", () => {
     console.log(`\n✅ ${client.user.username} is now online.`);
@@ -46,7 +19,7 @@ client.on("ready", () => {
 
 client.on("messageCreate", (msg) => {
     let msgOutput;
-
+    
     if (msg.author.bot) {
         return;
     }
@@ -66,7 +39,8 @@ client.on("messageCreate", (msg) => {
         let quoteNum = Math.floor(Math.random() * 25);
         msgOutput = quotes[quoteNum];
         msg.channel.send(msgOutput);
-        console.log(`${client.user.username} said quote #${quoteNum++}`);
+        quoteNum += 1;
+        console.log(`${client.user.username} said quote #${quoteNum}`);
         return;
     }
     if (msg.content === ":3") {
@@ -77,4 +51,4 @@ client.on("messageCreate", (msg) => {
     }
 });
 
-client.login(process.env.TOKEN);
+client.login(config.token);
---
+++ src/quotes.js
@@ -0,0 +1,32 @@
+const quotes = [
+    // amelia quotes (21)
+    "> everything that is ballshaped will be consumed \n\n--amelia",
+    ">>> NATALIE \n\ndo not \n\neat \n\ndilators \n\n--amelia",
+    "> the tattoo could just end up being up ur pussy \n\n--amelia",
+    "> like just a pile of boobs \n\n--amelia",
+    "> like can human bodies even DIGEST fetuses \n\n--amelia",
+    "> i didnt know wanting boobs can kill the entirety of europe \n\n--amelia",
+    "> i guess u just need breeding /j \n\n--amelia",
+    "> (it just releases gocks everywhere) \n\n--amelia",
+    "> thats bc you'd be using this only inside nattie \n\n--amelia",
+    "> the julie does NOT know about my habit of eating kids \n\n--amelia",
+    "> just dont put it in \n\n--amelia",
+    "> FOOT SERVER???? [#Unknown] \n\n--amelia",
+    "> bro just invented edible cock \n\n--amelia",
+    "> im going to shit myself \n\n--amelia",
+    "> ok i busted out the foot \n\n--amelia",
+    "> its just pink hole \n\n--amelia",
+    "> MAYBE this head isnt good \n\n--amelia",
+    "> and pussy hair. cant forget pussy hair. \n\n--amelia",
+    '> ig theyd have their dreams of genocide "ruined" by sex \n\n--amelia',
+    "> how would have they uh \n\n> gotten inside \n\n--amelia",
+    "> pussy time :3c \n\n--amelia",
+
+    // natalie quotes (4)
+    "> i did NOT expect to be talking abt blending stolen fetuses today \n\n--natalie",
+    "> wait wtf why are there like holes \n\n--natalie",
+    "> 129 insertions(+) :weary: \n\n--natalie",
+    "> i mean he had a fkn glass jar pop inside his ass and he didnt even SAY anything \n\n--natalie",
+];
+
+module.exports = quotes;
