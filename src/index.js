5d93f9810e7a2502c1ece6a6a3ca213b42f8ab84
Nyatalieeee <nyatalieeee@proton.me>
2024-06-09 14:10:12 -0500

    added command for listing all quotes

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ client.on("messageCreate", (msg) => {
         return;
     }
     if (msg.content === "!help") {
-        msgOutput = "- \"**!help**\" -> shows the list of commands\n\n- \"**!ping**\" -> responds with \"pong!\"\n\n- \"**!foo**\" -> responds with \"bar\"\n\n- \"**!quote**\" -> sends a random OOCQC Plus DMs quote\n\n- \"**:3**\" -> :3\n\n";
+        msgOutput = "- \"**!help**\" -> shows the list of commands\n\n- \"**!ping**\" -> responds with \"pong!\"\n\n- \"**!foo**\" -> responds with \"bar\"\n\n- \"**!quote**\" -> sends a random OOCQC Plus DMs quote\n\n- \"**!allquotes**\" -> sends a list of every OOCQC Plus DMs quote (use sparingly)\n\n- \"**:3**\" -> :3\n\n";
         msg.channel.send(msgOutput);
         console.log(`\n${client.user.username} sent the help message`);
         return;
@@ -42,9 +42,15 @@ client.on("messageCreate", (msg) => {
     if (msg.content === "!quote") {
         let quoteNum = Math.floor(Math.random() * 25);
         msgOutput = quotes[quoteNum];
-        msg.channel.send(msgOutput);
+        msg.channel.send(`> ${msgOutput}`);
         quoteNum += 1;
         console.log(`\n${client.user.username} said quote #${quoteNum}`);
+        return;
+    }
+    if (msg.content === "!allquotes") {
+        msgOutput = quotes.join('\n');
+        msg.channel.send(`\`\`\`${msgOutput}\`\`\``);
+        console.log(`\n${client.user.username} said every quote`);
         return;
     }
     if (msg.content === ":3") {
