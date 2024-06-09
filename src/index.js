23ed05638f62bb5034f9a50c6bc3df27bfff1089
Nyatalieeee <nyatalieeee@proton.me>
2024-06-09 10:16:04 -0500

    removed function-ception

--- src/index.js
+++ src/index.js
@@ -18,19 +18,22 @@ client.on("ready", () => {
 client.on("messageCreate", (msg) => {
     let msgOutput;
 
-    function ping() {
+    if (msg.author.bot) {
+        return;
+    }
+    if (msg.content === "!ping") {
         msgOutput = "pong!";
         msg.channel.send(msgOutput);
         console.log(`\n${client.user.username} said: "${msgOutput}"`);
         return;
     }
-    function foo() {
+    if (msg.content === "!foo") {
         msgOutput = "bar";
         msg.channel.send(msgOutput);
         console.log(`\n${client.user.username} said: "${msgOutput}"`);
         return;
     }
-    function quote() {
+    if (msg.content === "!quote") {
         let quoteNum = Math.floor(Math.random() * 25);
         msgOutput = quotes[quoteNum];
         msg.channel.send(msgOutput);
@@ -38,27 +41,11 @@ client.on("messageCreate", (msg) => {
         console.log(`\n${client.user.username} said quote #${quoteNum}`);
         return;
     }
-    function colonThree() {
+    if (msg.content === ":3") {
         msgOutput = ":3";
         msg.channel.send(msgOutput);
         console.log(`\n:3`);
         return;
-    }
-    
-    if (msg.author.bot) {
-        return;
-    }
-    if (msg.content === "!ping") {
-        return ping();
-    }
-    if (msg.content === "!foo") {
-        return foo();
-    }
-    if (msg.content === "!quote") {
-        return quote();
-    }
-    if (msg.content === ":3") {
-        return colonThree();
     }
 });
 
