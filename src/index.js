42aaf6a871c64f384257955c082d9888d3f32cb5
Nyatalieeee <nyatalieeee@proton.me>
2024-06-10 11:56:15 -0500

    THIS SHIT DOESNT WORK YET

--- src/index.js
+++ src/index.js
@@ -17,13 +17,14 @@ client.on("ready", () => {
 
 client.on("messageCreate", (msg) => {
     const rps = ["rock", "paper", "scissors"]; // TODO: rock paper scissors (jfc what do i even do with this)
+    let rpsInput = msg.content.split(' ')
     let msgOutput;
 
     if (msg.author.bot) {
         return;
     }
     if (msg.content === "!help") {
-        msgOutput = "- \"**!help**\" -> shows the list of commands\n\n- \"**!ping**\" -> responds with \"pong!\"\n\n- \"**!foo**\" -> responds with \"bar\"\n\n- \"**!quote**\" -> sends a random OOCQC Plus DMs quote\n\n- \"**!allquotes**\" -> sends a list of every OOCQC Plus DMs quote (use sparingly)\n\n- \"**:3**\" -> :3\n\n";
+        msgOutput = "- \"**!help**\" -> shows the list of commands\n\n- \"**!ping**\" -> responds with \"pong!\"\n\n- \"**!foo**\" -> responds with \"bar\"\n\n- \"**!quote**\" -> sends a random OOCQC Plus DMs quote\n\n- \"**!allquotes**\" -> sends a list of every OOCQC Plus DMs quote (use sparingly)\n\n- \"**!rps <'rock', 'paper', 'scissors'>**\" -> play rock paper scissors (doesnt fully work yet)\n\n- \"**:3**\" -> :3\n\n";
         msg.channel.send(msgOutput);
         console.log(`\n${client.user.username} sent the help message`);
         return;
@@ -59,6 +60,29 @@ client.on("messageCreate", (msg) => {
         msg.channel.send(msgOutput);
         console.log(`\n:3`);
         return;
+    }
+    // blame amelia for this /lh
+    if (rpsInput[0] === '!rps') {
+        if (rpsInput[1] === rps[0] || rpsInput[1] === rps[1] || rpsInput[1] === rps[2]) {
+            let rpsNum = Math.floor(Math.random() * rps.length);
+            msgOutput = rps[rpsNum];
+
+            if (msgOutput === rpsInput[1]) {
+                msg.channel.send(`${msgOutput}\n\nit's a tie!`);
+                console.log(`\n${client.user.username} said ${rps[rpsNum]}`);
+                return;
+            } else {
+                // if (msgOutput === rps[0] && something else idfk) {
+                //     end me
+                // }
+            }
+            return;
+
+        } else if (rpsInput[1] === "cutie") {
+            msgOutput = "gayass"
+            msg.channel.send(msgOutput);
+            return;
+        }
     }
 });
 
